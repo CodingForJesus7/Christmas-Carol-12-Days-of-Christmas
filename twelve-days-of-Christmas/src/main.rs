026d5d6b4de43306d8c4c8722b874475acbf64f0
CodingForJesus7 <parkerharris06@yahoo.com>
2026-08-12 18:47:58 -0500

    Edit code design
    I changed the design of the program once more. My notes should be of use
    when attempting to discern my thinking.

--- twelve-days-of-Christmas/src/main.rs
+++ twelve-days-of-Christmas/src/main.rs
@@ -1,23 +1,18 @@
 fn main() {
-    let day = [
-        "On the first day of Christmas my true love sent to me a partridge in a pear tree.", 
-        "second", 
-        "third", 
-        "fourth", 
-        "fifth", 
-        "sixth", 
-        "seventh", 
-        "eighth", 
-        "nineth",
-        "tenth", 
-        "eleventh", 
-        "twelfth",
-    ];
+    //List of 12 days
+    let mut one = "a partridge in a pear tree.";
+    let two = "Two turtle doves,";
 
     //"index" is the index number for "day"
-    let mut index
+    let mut index: u32 = 0;
     loop {
-
+        index += 1;
+        if index > 1 {
+            let one = "And a partridge in a pear tree.";
+        }
         println!("")
     }
 }
+
+//note to self: learn how to add "new line" for each sentence (e.g. one{new_line}two{new_line}three
+//and so on)
