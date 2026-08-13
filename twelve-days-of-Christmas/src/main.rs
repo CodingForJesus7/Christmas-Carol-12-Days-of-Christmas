28ae42ad21eb486b72600f401ba71fc9f42606a7
CodingForJesus7 <parkerharris06@yahoo.com>
2026-08-12 20:20:33 -0500

    Finish list of days list
    I also added an if statement for the 12th count in the loop.

--- twelve-days-of-Christmas/src/main.rs
+++ twelve-days-of-Christmas/src/main.rs
@@ -2,14 +2,30 @@ fn main() {
     //List of 12 days
     let mut one = "a partridge in a pear tree.";
     let two = "Two turtle doves,";
+    let three = "Three French hens,";
+    let four = "Four calling birds,";
+    let five = "Five golden rings,";
+    let six = "Six geese a-laying,";
+    let seven = "Seven swans a-swimming,";
+    let eight = "Eight maids a-milking,";
+    let nine = "Nine ladies dancing,";
+    let ten = "Ten lords a-leaping,";
+    let eleven = "Eleven pipers piping,";
+    let twelve = "Twelve drummers drumming,";
 
     //"index" is the index number for "day"
     let mut index: u32 = 0;
     loop {
         index += 1;
+
         if index > 1 {
             let one = "And a partridge in a pear tree.";
         }
+
+        if index == 12 {
+            let one = "And a partridge in a pear tree!";
+        }
+
         println!("")
     }
 }
